109dfa231e48cbbde36da9b9deacb0caa0af8e2c
Mauricio12-gif <lovermax876@gmail.com>
2026-07-31 19:27:39 +0300

    Implement message sending functionality

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
+import { db } from "./firebase.js";
 
+
+import {
+
+collection,
+addDoc,
+serverTimestamp
+
+} from 
+"https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+
+
+window.sendMessage = async function(){
+
+
+const message = 
+document.getElementById("message").value;
+
+
+
+if(message.trim()==""){
+
+alert("Write a message first");
+
+return;
+
+}
+
+
+
+await addDoc(
+
+collection(db,"messages"),
+
+{
+
+text:message,
+
+time:serverTimestamp()
+
+}
+
+);
+
+
+
+document.getElementById("status").innerHTML =
+"Message sent ❤️";
+
+
+document.getElementById("message").value="";
+
+
+}
