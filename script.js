dce425f758d5c0e849343b63ded50ae864dc7d1a
Mauricio12-gif <lovermax876@gmail.com>
2026-07-31 20:14:38 +0300

    Refactor sendMessage function for Firebase integration

--- script.js
+++ script.js
@@ -4,37 +4,75 @@ import {
     collection,
     addDoc,
     serverTimestamp
-} from 
-"https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
-
-
-// Test Firebase loading
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
 
 console.log("SecretTalk JavaScript loaded");
 
 
-
-// Send anonymous message
+// SEND ANONYMOUS MESSAGE
 
 window.sendMessage = async function(){
 
-
     const messageInput = document.getElementById("message");
-
     const status = document.getElementById("status");
 
-
     const message = messageInput.value.trim();
-
 
 
     if(message === ""){
 
         alert("Write a message first");
-
         return;
 
     }
+
+
+    try{
+
+        await addDoc(
+
+            collection(db, "messages"),
+
+            {
+
+                text: message,
+
+                sender: "Anonymous",
+
+                time: serverTimestamp()
+
+            }
+
+        );
+
+
+        status.innerHTML =
+        "Message sent successfully ❤️";
+
+
+        messageInput.value = "";
+
+
+        console.log("Message saved");
+
+    }
+
+
+    catch(error){
+
+
+        console.log("Firebase error:", error);
+
+
+        status.innerHTML =
+        "Error sending message ❌";
+
+
+        alert(error.message);
+
+    }
+
+};    }
 
 
 
